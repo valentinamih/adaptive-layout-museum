5c0af59b6176d87a3e83900fa329e2341c618583
Valya <vally-arrty@mail.ru>
2022-02-23 17:14:31 +0300

    add project files

--- museum-dom/js/script.js
+++
@@ -1,8 +0,0 @@
-const progress = document.querySelectorAll('.progress');
-
-for (let item of progress) {
-    item.addEventListener('input', function() {
-        const value = this.value;
-        this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
-    })
-}
